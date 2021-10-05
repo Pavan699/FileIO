f6b3d32a6f200d3e1b3440ca274100f57bab3dc8
Pavan699 <pavannakate03@gmail.com>
2021-10-05 01:55:33 -0700

    read,write Methods and streamReader and streamWriter methods

---
+++ FileInputOutput/IOReadWrite.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace FileInputOutput
+{    
+    class IOReadWrite
+    {
+        internal const string ReadFilePath = @"D:\VS .net\FileInputOutput\FileInputOutput\ReadFileData.txt";
+        internal const string WriteFilePath = @"D:\VS .net\FileInputOutput\FileInputOutput\WriteFileData.txt";
+
+        public static void ReadData()
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++Read-File+++++++++++++++++++++++++++++++++");
+            Console.WriteLine( File.ReadAllText(ReadFilePath));
+            Console.WriteLine("**********************************************************************");
+        }
+        public static void WriteData(string inputdata)
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++Write-File+++++++++++++++++++++++++++++++++");
+            File.AppendAllText(WriteFilePath, inputdata);
+            Console.WriteLine("String is Added");          
+            Console.WriteLine("**********************************************************************");
+        }
+
+        public static void StreamReadData()
+        {
+            //Without "using" keyword
+            Console.WriteLine("+++++++++++++++++++++++++StreamReader-File++++++++++++++++++++++++++++++");
+            Console.WriteLine("Without 'using' keyword : ");
+            StreamReader streamReader = new StreamReader(ReadFilePath);
+            Console.WriteLine(streamReader.ReadToEnd());           
+            Console.WriteLine("**********************************************************************");
+            Console.WriteLine("With 'using' Keyword");
+            using (StreamReader streamReaderusing = new StreamReader(ReadFilePath))
+            {
+                while (!streamReaderusing.EndOfStream)
+                {
+                    Console.WriteLine(streamReaderusing.ReadLine());
+                }
+            }
+            streamReader.Close();
+        }
+        public static void StreamWriteData(string data)
+        {
+            Console.WriteLine("++++++++++++++++++++++++StreamWriter-File++++++++++++++++++++++++++++++");           
+            StreamWriter streamWriter = new StreamWriter(WriteFilePath,true);
+            streamWriter.WriteLine(data);
+            Console.WriteLine("Data is Added");
+            streamWriter.Close();
+            Console.WriteLine("**********************************************************************");
+        }
+    }
+}
--- FileInputOutput/Program.cs
+++ FileInputOutput/Program.cs
@@ -7,6 +7,34 @@ namespace FileInputOutput
         static void Main(string[] args)
         {
             Console.WriteLine("***********************Welcome to FileIO Program***********************");
+            int count = 1;
+            while(count!=2)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1.ReadData 2.WriteData 3.StreamReader 4.StreamWriter 5.Exit");
+                Console.Write("Enter The Choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        IOReadWrite.ReadData();
+                        break;
+                    case 2:
+                        IOReadWrite.WriteData("\nCollections,Json,Nlog,Reflections,Anotations,FileIO,etc...");
+                        break;
+                    case 3:
+                        IOReadWrite.StreamReadData();
+                        break;
+                    case 4:
+                        string data = "\n In Data-Structures : Linked-List,Array-List,Hash-set,Hash-Map,BST,Dictionries,Stack,Queue,etc....";
+                        IOReadWrite.StreamWriteData(data);
+                        break;
+                    default:
+                        Console.WriteLine("Worng Input :)");
+                        count++;
+                        break;
+                }
+            }           
         }
     }
 }
